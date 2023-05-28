e3a4217c459a2f1275ec622ea4a90616830252ef
danielpemor123 <127167442+danielpemor123@users.noreply.github.com>
2023-05-28 14:32:34 -0600

    Update exercise.py

--- assignments/01CmaKmMtCm/src/exercise.py
+++ assignments/01CmaKmMtCm/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
-
+n = int(input('Introduce los cm: '))
+kilometros = n // 10000
+metros = (n // 100) % 1000
+centimetros = n % 100
+print("Resultado:")
+if kilometros != 0 :
+    print("km:", kilometros)
+if metros != 0:
+    print("m:", metros)
+if centimetros != 0:
+        print("cm:", centimetros)
 if __name__ == '__main__':
     main()
